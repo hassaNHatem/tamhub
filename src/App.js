a4f1a8fface1e13c3a81bbb4b3fd1ed1cb06f636
hassaNHatem <hassan.hatem.44@gmail.com>
2022-09-13 06:06:50 +0200

    added initial routing structure

--- src/App.js
+++ src/App.js
@@ -2,15 +2,36 @@ import './App.css';
 import Dashboard from './dashboard/Dashboard';
 import Login from './login/Login';
 import Cars from './cars/Cars';
-import SideBar from './sidebar/SideBar';
-
+import Layout from './Layout/Layout';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from "react-router-dom";
 function App() {
+  localStorage.setItem('person','soemthing')
+  localStorage.clear()
+  const user = localStorage.getItem('person')
+  console.log(user)
   return (
     <div className="App">
-     <Login/>
-     <SideBar/>
-     <Dashboard/>
-     <Cars/>
+      <Router>
+      {user === null ? (
+          <Routes>
+            <Route path="*" element={<Navigate to="/" />} />
+            <Route path="/" element={<Login />} />
+          </Routes>
+        ) : (
+          <Routes>
+      <Route path="/" element={ <><Layout/><Dashboard/></>} />
+      <Route path="/dashboard" element={  <><Layout/><Dashboard/></>} />
+      <Route path="/cars" element={  <><Layout/><Cars/></>} />
+      </Routes>
+      )}
+      </Router>
+    
+    
     </div>
   );
 }
---
+++ src/Layout/Layout.js
@@ -0,0 +1,11 @@
+import './layout.css';
+
+function Layout() {
+  return (
+    <div className="App">
+    layout
+    </div>
+  );
+}
+
+export default Layout;
