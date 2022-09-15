647d54ef11336730311e09beef2dddf29a74ba24
hassaNHatem <hassan.hatem.44@gmail.com>
2022-09-15 04:08:09 +0200

    applied filters on mockdata cars page

--- src/cars/Cars.js
+++ src/cars/Cars.js
@@ -1,25 +1,64 @@
+import { useEffect, useState } from 'react';
 import CarsGrid from '../components/CarsGrid';
 import './cars.css';
 import {squares , filter , arrow} from './svgs'
 function Cars({cars}) {
+  const [gear , setGear] = useState('all gears')
+  const [company , setCompany] = useState('all')
+  const [filteredCars , setFilteredCars] = useState(cars)
+  let carsCompanies = []
+  cars.map(el=>{
+    carsCompanies.push(el.name)
+  })
+  const set = new Set(carsCompanies);
+  let uniqueCompanies = [...set]
+  useEffect(()=>{
+    setFilteredCars(cars)
+  },[cars])
+  useEffect(()=>{
+    applyfilters(gear , company)
+  },[gear , company])
+  function applyfilters(gear , company){
+    if(gear==='all gears'&& company==='all'){
+      setFilteredCars(cars)
+    }else if(gear==='all gears'&& company!=='all'){
+      let filteredArr = cars.filter(el=>{
+        return el.name===company
+      })
+      setFilteredCars(filteredArr)
+    }else if(gear!=='all gears'&& company==='all'){
+      let filteredArr = cars.filter(el=>{
+        return el.trass===gear
+      })
+      setFilteredCars(filteredArr)
+    }else{
+      let filteredArr = cars.filter(el=>{
+        return el.trass===gear&&el.name===company
+      })
+      setFilteredCars(filteredArr)
+    }
+  }
+  console.log(filteredCars)
   return (
     <div className="cars-wrapper">
     <h1 className='heading'>Booking</h1>
     <div className='filters'>
       <div className='dropdowns'>
         <div className='select-wrapper'>
-        <select>
-          <option>All</option>
-          <option>new</option>
-          <option>used</option>
+         
+        <select onChange={(e)=>{setGear(e.target.value)}} value={gear}>
+          <option value={'all gears'}>All Gears</option>
+          <option value={'manual'}>manual</option>
+          <option value={'Automatic'}>Automatic</option>
         </select>
         <p>{arrow}</p>
         </div>
         <div className='select-wrapper'>
-        <select>
-          <option>All</option>
-          <option>Toyota</option>
-          <option>Pourche</option>
+        <select onChange={(e)=>{setCompany(e.target.value)}} value={company}>
+          <option value={'all'}>All</option>
+          {uniqueCompanies.map(el=>{
+            return <option value={el}>{el}</option>
+          })}
         </select>
         <p>{arrow}</p>
         </div>
@@ -30,7 +69,7 @@ function Cars({cars}) {
         <h4 style={{'height':'30px'}}>{filter}</h4>
       </div>
     </div>
-    <CarsGrid cars={cars}/>
+    <CarsGrid cars={filteredCars} />
     </div>
   );
 }
--- src/components/CarsGrid.js
+++ src/components/CarsGrid.js
@@ -1,7 +1,6 @@
 import CarCard from "./CarCard"
 
 function CarsGrid({cars}) {
-    console.log(cars)
     return(
         <div className="cars-grid">
             {cars.map(el=>{
